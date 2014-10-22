12c0a6b293e8c947a59b26ef2b54523ad5aaf0ce
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2014-10-22 14:11:21 -0500

    Added fill mode to rect. Changed layout format.

--- src/net/teslaworks/visualizer/shapes/Rectangle.java
+++ src/net/teslaworks/visualizer/shapes/Rectangle.java
@@ -12,19 +12,24 @@ public class Rectangle extends Shape {
     // Shape size
     public final int width;
     public final int height;
-    public final boolean fillMode;
+    public final boolean fill;
 
     // Set values unique to rectangles
     protected Rectangle(Element e) {
         super(e);
         width = Integer.parseInt(e.attributeValue("width"));
         height = Integer.parseInt(e.attributeValue("height"));
-        fillMode = "fill".equals(e.attributeValue("mode"));
+        fill = Boolean.parseBoolean(e.attributeValue("fill"));
     }
 
     // Draw this rectangle
     public void paint(Graphics2D g2d, int channelValue) {
         super.paint(g2d, channelValue);
-        g2d.fillRect(x, y, width, height);
+        if (fill) {
+            g2d.fillRect(x, y, width, height);
+        }
+        else {
+            g2d.drawRect(x, y, width, height);
+        }
     }
 }
