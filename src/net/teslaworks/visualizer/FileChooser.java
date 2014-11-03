762883236a31bb05ec575830d4bd31d546d5b93c
Ryan Smith <ryansmith125@gmail.com>
2014-11-03 14:57:41 -0600

    INCOMPLETE: Started porting file chooser from previous version On branch file-dialogue-testing Changes to be committed: 	new file:   src/net/teslaworks/visualizer/FileChooser.java 	modified:   src/net/teslaworks/visualizer/Visualizer.java

---
+++ src/net/teslaworks/visualizer/FileChooser.java
@@ -0,0 +1,39 @@
+package net.teslaworks.visualizer;
+
+import java.awt.Component;
+import java.awt.HeadlessException;
+import java.io.File;
+
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+
+public class FileChooser {
+	private File file;
+	public void choose() {
+        JFileChooser fc = new JFileChooser() {				
+        	private static final long serialVersionUID = 1170852534375941586L;
+            protected JDialog createDialog(Component parent)
+                    throws HeadlessException {
+                JDialog dialog = super.createDialog(parent);
+                // config here as needed - just to see a difference
+                dialog.setLocationByPlatform(true);
+                // might help - can't know because I can't reproduce the problem
+                dialog.setAlwaysOnTop(true);
+                return dialog;
+            }
+
+        };
+        int retValue = fc.showOpenDialog(null);
+        if (retValue == JFileChooser.APPROVE_OPTION){
+            file = fc.getSelectedFile();
+        } else {
+            file = null;
+        }
+    }
+	public File getFile() {
+		return file;
+	}
+	public String getFilename() {
+		return file.getPath();
+	}
+}
--- src/net/teslaworks/visualizer/Visualizer.java
+++ src/net/teslaworks/visualizer/Visualizer.java
@@ -1,6 +1,7 @@
 package net.teslaworks.visualizer;
 
 import java.awt.EventQueue;
+import java.io.File;
 import java.util.List;
 
 import javax.swing.SwingUtilities;
@@ -12,18 +13,37 @@ import org.dom4j.Document;
 
 public class Visualizer {
 
-    // Filename to read layout data from.
-    private static final String LAYOUT_FILENAME = "/layout1.xml";
+    // File to read layout data from.
+	private FileChooser filechooser = new FileChooser();
+	private static String LAYOUT_FILENAME = "/layout1.xml";
 
     public static void main(String[] args) throws Exception {
         // Get vixen log file from command line
-        final String targetFilename = args[0];
-        
+        String vltarget="";
+        for (int i=0; i<args.length; i++) {
+        	if (args[i] == "-tf") {
+        		i++;
+        		LAYOUT_FILENAME = args[i];
+        	}
+        	if (args[i] == "-vt") {
+        		i++;
+        		vltarget = args[i];
+        	}
+        }
+        if (vltarget == "") {
+        	System.err.println("ERROR: No Vixen target file specified. Exiting");
+        	System.exit(1);
+        }
+        if (LAYOUT_FILENAME == "") {
+        	
+        }
         // LayoutXML has constants with all layout data after parse
         final LayoutXML layout = new LayoutXML(LAYOUT_FILENAME);
 
         // Launch the display, passing the layout data
+        final String targetFilename = vltarget;
         SwingUtilities.invokeLater(new Runnable() {
+        	
             @Override
             public void run() {
                 VizFrame frame = new VizFrame(layout, targetFilename);
