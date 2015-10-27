4e03e49be9b2938d78248f1c65c89c7d4e7f4179
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-10-27 01:51:01 -0500

    net light channels work. needs bush pattern

--- src/net/teslaworks/visualizer/shapes/NetLight.java
+++ src/net/teslaworks/visualizer/shapes/NetLight.java
@@ -9,6 +9,7 @@ public class NetLight extends Shape {
     // Shape size
     private final int width, height;
     private final int xCount, yCount, gap;
+    private final boolean reverseCheckered;
 
     // Set values unique to rectangles
     protected NetLight(Element e) {
@@ -18,14 +19,18 @@ public class NetLight extends Shape {
         xCount = Integer.parseInt(e.attributeValue("xCount"));
         yCount = Integer.parseInt(e.attributeValue("yCount"));
         gap = Integer.parseInt(e.attributeValue("gap"));
+        reverseCheckered = Boolean.parseBoolean(e.attributeValue("reverse"));
     }
 
     // Draw this rectangle
     public void paintWork(Graphics2D g2d, int[] channelValues) {
+        int c = channel;
         for (int i = 0; i < xCount; i++) {
-            for (int j = 0; j < yCount; j++) {
+            for (int j = yCount - 1; j >= 0; j--) {
+                g2d.setPaint(new Color(red, green, blue, channelValues[c]));
                 g2d.fill(new java.awt.Rectangle(
                         x + i * (width + gap), y + j * (height + gap), width, height));
+                c++;
             }
         }
     }
