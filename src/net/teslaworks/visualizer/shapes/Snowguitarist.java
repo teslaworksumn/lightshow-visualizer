78100387e5adbc949ff9841070f0c597eb8a9aea
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-10-30 14:51:10 -0500

    Snowguitarist DONE

--- src/net/teslaworks/visualizer/shapes/Snowguitarist.java
+++ src/net/teslaworks/visualizer/shapes/Snowguitarist.java
@@ -10,23 +10,25 @@ public class Snowguitarist extends Shape {
     // Shape size
     public final int BASE_HEIGHT = 80;
     private final int height;
+    private final double scale;
 
     // Set values unique to rectangles
     protected Snowguitarist(Element e) {
         super(e);
         height = Integer.parseInt(e.attributeValue("height"));
+        scale = (1.0 * height) / BASE_HEIGHT;
     }
 
     // Draw this oval
     public void paintWork(Graphics2D g2d, int[] channelValues) {
         g2d.translate(x, y);
-        g2d.scale(1, (1.0 * height) / BASE_HEIGHT);
+        g2d.scale(scale, scale);
 
         paintBodyWork(g2d);
         paintGuitarWork(g2d, channelValues);
         paintArmsWork(g2d, channelValues);
 
-        g2d.scale(1, (1.0 * BASE_HEIGHT) / height);
+        g2d.scale(1.0 / scale, 1.0 / scale);
         g2d.translate(0 - x, 0 - y);
     }
 
@@ -39,12 +41,25 @@ public class Snowguitarist extends Shape {
     }
 
     private void paintArmsWork(Graphics2D g2d, int[] channelValues) {
-        // right drumming arm
-        g2d.setPaint(new Color(255, 255, 0, channelValues[channel + 0]));
-
+        // right arm up
+        g2d.setPaint(new Color(255, 255, 0, channelValues[channel + 1]));
         g2d.drawLine(13, 25, 3, 38);
         g2d.drawLine(3, 38, 20, 35);
+
+        // right arm down
+        g2d.setPaint(new Color(255, 255, 0, channelValues[channel + 2]));
+        g2d.drawLine(13, 25, 3, 38);
         g2d.drawLine(3, 38, 20, 55);
+
+        // left arm up
+        g2d.setPaint(new Color(255, 255, 0, channelValues[channel + 3]));
+        g2d.drawLine(27, 25, 45, 50);
+        g2d.drawLine(45, 50, 65, 29);
+
+        // left arm down
+        g2d.setPaint(new Color(255, 255, 0, channelValues[channel + 4]));
+        g2d.drawLine(27, 25, 45, 50);
+        g2d.drawLine(45, 50, 50, 33);
     }
 
     private void paintGuitarWork(Graphics2D g2d, int[] channelValues) {
