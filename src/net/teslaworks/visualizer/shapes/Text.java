2932856d5fa6a3622d1cf6d162189700b4a6c071
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-11-14 20:51:29 -0600

    Added UMN. Layout done. Needs testing and channels

--- src/net/teslaworks/visualizer/shapes/Text.java
+++ src/net/teslaworks/visualizer/shapes/Text.java
@@ -8,20 +8,20 @@ import org.dom4j.Element;
 public class Text extends Shape {
     
     public final String value;
-    public final String font;
-    public final int fontSize;
+    public final Font font;
 
     // Set values unique to rectangles
     protected Text(Element e) {
         super(e);
         value = e.getTextTrim();
-        font = e.attributeValue("font", "Purisa");
-        fontSize = Integer.parseInt(e.attributeValue("fontSize", "12"));
+        String font = e.attributeValue("font", "Purisa");
+        int fontSize = Integer.parseInt(e.attributeValue("fontSize", "12"));
+        this.font = new Font(font, Font.PLAIN, fontSize);
     }
 
     // Draw this rectangle
     public void paintWork(Graphics2D g2d, int[] channelValues) {
-        g2d.setFont(new Font(font, Font.PLAIN, fontSize));
+        g2d.setFont(font);
         g2d.drawString(value, x, y);
     }
 }
